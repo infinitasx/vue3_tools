826bf139f506cbdafc2694885781b5f6add95cd4
huyanhai <810839700@qq.com>
2020-11-26 11:30:04 +0800

    测试 lint-staged

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import element from '../package/index';
 const app = createApp(App);
 
-app.config.isCustomElement = tag => tag.startsWith('apps-');
+// app.config.isCustomElement = tag => tag.startsWith('apps-');
 
-app.use(element);
 app.use(store);
 app.use(router);
 
