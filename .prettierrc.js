ffaa2901f2d7d9da7d03145caeed7041afcd494b
huyanhai <810839700@qq.com>
2020-11-30 10:02:16 +0800

    test: test commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -7,8 +7,5 @@ module.exports = {
     trailingComma: 'all', // 多行使用拖尾逗号（默认none）
     bracketSpacing: true, // 对象字面量的大括号间使用空格（默认true）
     jsxBracketSameLine: false, // 多行JSX中的>放置在最后一行的结尾，而不是另起一行（默认false）
-    // 箭头函数参数括号 默认avoid 可选 avoid| always
-    // avoid 能省略括号的时候就省略 例如x => x
-    // always 总是有括号
     arrowParens: 'avoid',
 };
